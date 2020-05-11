51278a1c7a8dff6273e925d305e600beab011cba
lucia-gomez <isabel.lucia.gomez@gmail.com>
2020-05-10 22:51:49 -0700

    changed to line graph

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,66 +1,7 @@
 import React from "react";
 import AboutSection from "./about-section"
 import LangBars from "./lang-bars"
-import Doughnut from "react-chartjs-2"
-
-const chartData1 = {
-  labels: ['Homework', 'Uber Eats', 'Re-reading Harry Potter', 'Pokemon Go', 'Building this website'],
-  datasets: [
-    {
-      data: [5, 10, 15, 25, 45],
-      backgroundColor: [
-        '#f7868e',
-        '#a8dadc',
-        '#457b9d',
-        '#1d3557',
-        '#e63946'
-      ],
-      hoverBackgroundColor: [
-        '#faafb4',
-        '#bae4e6',
-        '#5190b8',
-        '#304f7a',
-        '#eb6570'
-      ],
-    }
-  ]
-}
-
-const chart1 = (<Doughnut data={chartData1}
-  options={{
-    aspectRatio: 1,
-    responsive: true,
-  }}
-  legend={{
-    "display": false,
-    "position": "right",
-    "fullWidth": false,
-    "reverse": true,
-    "labels": {
-      "fontColor": "#1d3557",
-      "fontFamily": 'Roboto',
-    }
-  }}
-  height={null}
-  width={null} />);
-
-function buildLegend(chart) {
-  const data = chart.props.data
-  const items = [];
-  for (var i = 0; i < data.datasets[0].data.length; i++) {
-    items.push(
-      <div style={{ backgroundColor: data.datasets[0].backgroundColor[i] }} className='legend-item' key={i}>
-        {data.labels[i] ? data.labels[i] : ''}
-      </div>
-    )
-  }
-  items.reverse();
-  return (
-    <div id='chart1-legend'>
-      {items}
-    </div>
-  );
-}
+import LineGraph from "./line-graph"
 
 const About = () => (
   <div>
@@ -81,11 +22,8 @@ const About = () => (
         <LangBars />
         <p id='scratch-note'>* My Scratch experience surpasses this scale</p>
       </AboutSection>
-      <AboutSection title="How I'm Spending My Quarantine">
-        <div id='chart-area'>
-          <div className='chart'>{chart1}</div>
-          {buildLegend(chart1)}
-        </div>
+      <AboutSection title="My Quarantine Activities Over Time">
+        <LineGraph />
       </AboutSection>
     </div>
   </div>
---
+++ src/components/about/line-graph.js
@@ -0,0 +1,67 @@
+import React from "react";
+import { Line } from "react-chartjs-2"
+
+function dataset(data, label, color) {
+  return {
+    label: label,
+    data: data,
+    borderColor: color,
+    borderWidth: 5,
+    fill: false,
+    pointBorderWidth: 1,
+    pointHoverRadius: 5,
+    pointRadius: 1,
+    pointHitRadius: 10,
+    borderJoinStyle: 'miter',
+    clip: { left: 0, top: 10, right: 0, bottom: 0 }
+  };
+}
+
+const data = {
+  labels: Array.from(Array(10).keys()),
+  datasets: [
+    dataset([90, 90, 50, 0, 0, 0, 0, 0, 0, 0], 'Jogging', '#a8dadc'),
+    dataset([20, 20, 20, 20, 20, 20, 20, 20, 20, 20], 'Anxiously refreshing NYTimes', '#457b9d'),
+    dataset([0, 0, 0, 0, 0, 0, 0, 0, 60, 70], 'Re-reading Harry Potter', '#1d3557'),
+    dataset([30, 30, 40, 50, 80, 90, 100, 100, 80, 100], 'Building this website', '#e63946'),
+    dataset([60, 60, 50, 50, 50, 50, 60, 60, 50, 40], 'Pokemon Go', '#f7868e'),
+  ]
+};
+
+const LineGraph = () => (<Line
+  data={data}
+  options={{
+    // aspectRatio: 2,
+    maintainAspectRatio: false,
+    responsive: true,
+    scales: {
+      xAxes: [{
+        ticks: {
+          display: false
+        },
+      }],
+      yAxes: [{
+        ticks: {
+          display: false
+        },
+      }]
+    },
+    layout: {
+      padding: {
+        top: 5,
+        bottom: 0,
+      }
+    },
+  }}
+  legend={{
+    "display": true,
+    "position": "bottom",
+    "labels": {
+      "fontColor": "#1d3557",
+      "fontFamily": 'Roboto',
+    }
+  }} width={null}
+  height={400}
+/>);
+
+export default LineGraph
