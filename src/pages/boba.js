c9c0920ff5d6914fa5cc0ea26293c863ad396916
lucia-gomez <isabel.lucia.gomez@gmail.com>
2020-05-16 20:19:56 -0700

    removed tableau vizzes from boba page, added links

--- src/pages/boba.js
+++ src/pages/boba.js
@@ -1,33 +1,22 @@
 import React from "react";
 import Layout from "../components/layout";
+import { Button } from "react-bootstrap"
 
 class Boba extends React.Component {
-
-  url1 = "https://public.tableau.com/views/BubbleTeaBobaWordChoice/Sheet1?:display_count=y&:origin=viz_share_link";
-  url2 = "https://public.tableau.com/views/BubbleTeaBobaWordChoice/Heatmaps?:display_count=y&:origin=viz_share_link";
-
-  renderTableau(url) {
-    if (typeof window !== `undefined`) {
-      var Tableau = require("tableau-react");
-      return <div className='tableauDemo'><Tableau url={url} /></div>
-    }
-    return null;
-  }
 
   render() {
     return (
       <Layout>
         <section id="boba">
           <h1>Spill the Tea</h1>
-          <h2 style={{ marginBottom: '30px' }}>Survey Data Visualization</h2>
-          <p id="boba-description">What do you call this drink? I call it 'bubble tea' but it goes by many names,
-          similar to the famous soda/pop/coke divide. A friend and I decided to investigate this variation for a
+          <p id="boba-description">What do you call this drink? I call it 'bubble tea' but it goes by many names.
+          A friend and I decided to investigate this variation for a
           socio-linguistics research project. We conducted a study on the social factors that could influence word choice,
-        and surveyed over 200+ participants. The data below summarizes the survey results. </p>
-        </section>
-        <section id="boba-demos">
-          {this.renderTableau(this.url1)}
-          {this.renderTableau(this.url2)}
+        and surveyed over 200+ participants. Explore the results on Tableau, with interactive data visualizations.</p>
+          <div className='flex-horizontal' id='boba-buttons'>
+            <a href='https://public.tableau.com/profile/lucia.gomez#!/vizhome/BubbleTeaBobaWordChoice/Sheet1' target='_blank' rel="noreferrer"><Button>Demo choropleth maps</Button></a>
+            <a href='https://public.tableau.com/profile/lucia.gomez#!/vizhome/BubbleTeaBobaWordChoice/Heatmaps' target='_blank' rel="noreferrer"><Button>Demo heatmaps</Button></a>
+          </div>
         </section>
       </Layout>
     );
